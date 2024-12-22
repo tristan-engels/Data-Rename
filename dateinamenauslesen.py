138588b25a0721c60459deebb864c52e92f230b5
tristan-engels <trissi05.te@gmail.com>
2024-12-23 00:24:29 +0100

    Funktionen eingebaut, Multiple Dateien in Arbeit

--- dateinamenauslesen.py
+++ dateinamenauslesen.py
@@ -1,14 +1,27 @@
 import os
 from docx import Document
 
-while True:
+def main():
+    funktionauswahl = input("Gebe ein Welche Funktion des Programm du benutzen möchtest, \"1\" für ein Einzelnes Dokument: ")
+    if funktionauswahl == "1":
+        getsingledocument()
+    elif funktionauswahl == "2":
+        getmultpledocument()
+    else:
+        print("Gebe nur die Zahl 1 oder 2 ein")
+        main()
+
+def getsingledocument():
     while True:
+        global pfad
         pfad = input("geben sie den Dateipfad an wo sich die Datei befindet: ")
+        global dokument
         dokument = input("Gebe den vollen Dateinamen ein des Dokumentes: ")
         if not pfad.endswith("\\"):
             pfad += "\\"
         if not dokument.endswith(".docx"):
             dokument += ".docx"
+        global docpfad
         docpfad = pfad + dokument
 
         if os.path.exists(docpfad):
@@ -16,7 +29,33 @@ while True:
             break
         else:
             print(f"Der Angegebene Pfad: {pfad} oder die Datei {dokument} existiert nicht, gebe beides neu ein!")
+    renamedata()
 
+def getmultpledocument():
+    while True:
+        global pfad
+        pfad = input("Gebe den Ordnerpfad an wo alle Dateien umbenannt werden sollen: ")
+        if not pfad.endswith("\\"):
+            pfad += "\\"
+            break
+        if os.path.exists(pfad):
+            break
+        else:
+            print(f"der angegebene Pfad \"{pfad}\" existiert nicht, gebe ihn neu ein")
+    finddocx(pfad)
+
+def finddocx(pfad):
+    print("test")
+    global dateien
+    dateien = []
+    for root, _, dateinamen in os.walk(pfad):
+        for datei in dateinamen:
+            if datei.endswith('.docx'):
+                dateien.append(os.path.join(root, datei))
+    return dateien
+
+
+def renamedata():
     doc = Document(docpfad)
     print("Die Ersten 3 Wörter/Überschrifft sind:")
     first = doc.paragraphs[0].text
@@ -37,16 +76,19 @@ while True:
             break
         else:
             print("Ungültige Eingabe, gebe nur \"ja\" oder \"nein\"")
-    
+    getsingledocument2()
+
+def getsingledocument2():    
     while True:
         weitermachen2 = input('Möchtest du das noch eine andere Datei umbennant wird, gebe "ja" oder "nein" ein.').lower()
         if weitermachen2 == "ja":
             print("Das Programm wird fortgeführt!")
-            break
+            getsingledocument()
         elif weitermachen2 == "nein":
             print("Danke fürs benutzen.")
             exit()  
         else:
             print("Ungültige Eingabe, gebe nur \"ja\" oder \"nein\"")
 
-        #Test ob git Funktioniert
+if __name__ == "__main__":
+    main()
